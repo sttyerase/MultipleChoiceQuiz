383c4a15ed0c5a98d7c3bcd48974ec641e0c6e87
Dan Moore <sttyerase@gmail.com>
2021-04-05 23:06:33 -0400

    Java servlet components.

--- src/main/java/com/dbmi/demos/quiz/QuizManagerServlet.java
+++ src/main/java/com/dbmi/demos/quiz/QuizManagerServlet.java
@@ -2,43 +2,31 @@ package com.dbmi.demos.quiz;
 
 import javax.servlet.*;
 
-/**
-*/
-
-
 /** @author Daniel B. Moore, aka sttyerase@gmail.com <p>
  * <p>QuizManagerServlet (Multiple Choice Quiz)
- * <p>Public Licence
- * <p>Copyright (c) 1999-2000 Daniel B. Moore.  All rights reserved.
- *
- * <p>This program is free software; you can redistribute it and/or
- * modify it under the current terms of the GNU General Public License
- * as published by the Free Software Foundation (ref http://www.gnu.org).
- * <p><b>The QuizManagerServlet class provides the servlet interface for displaying XML documents of
- * type "Multiple Choice Quiz" to client browsers. This interface 
- * creates a quiz document for display in a browser from an XML document specified 
- * in the browser URL and creates forms for response to quiz questions.</b>
  **/
 public class QuizManagerServlet extends javax.servlet.http.HttpServlet
 {
    private String theDocument = null;  /** holds the name of the Quiz document
-                                         * to be parsed. **/  
+                                         * to be parsed. **/
    private Quiz myQuiz = null;
    private QuestionList qList = new QuestionList(300);
    private String quizPath = null;
 
-   /** Initiializes the QuizManagerServlet servlet.    */
+   /** Initializes the QuizManagerServlet servlet.    */
    
    public void init(ServletConfig config){
       try{
          super.init(config);
          quizPath = config.getInitParameter("quizpath");
+         config.getServletContext().setAttribute("manager", this);
+         config.getServletContext().log("Quiz servlet initialized.");
 	   	if(quizPath == null) quizPath = ".";
          quizPath += "/";
          config.getServletContext().log("Init values: quizpath=" + quizPath);
       }catch (ServletException se){
          log("Servlet Exception caught: " + se);
-      }
+      } // TRY-CATCH
    } // INIT
 
    /** RETURN A COMMENT ABOUT THE SERVLET. USED BY APPLICATION SERVERS TO DISPLAY
@@ -47,6 +35,6 @@ public class QuizManagerServlet extends javax.servlet.http.HttpServlet
 	public String getServletInfo()
 	{
 		return "A quiz Servlet that displays multiple choice questions from an XML document.";
-	}
+	} // GETSERVLETINFO()
 
 } // CLASS
--- src/main/webapp/Quiz.jsp
+++ src/main/webapp/Quiz.jsp
@@ -1,12 +1,21 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page contentType="text/html; charset=UTF-8"
+         import="com.dbmi.demos.quiz.*"
+         pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>Multiple Choice Quiz Application (MCQuiz)</title>
-</head>
-<body>
-
-</body>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link REL=StyleSheet HREF="QuizSite.css" TYPE="text/css"/>
+    <title>Multiple Choice Quiz Application (MCQuiz)</title>
+    <%
+    String thePath = config.getServletContext().getRealPath("/");
+    config.getServletContext().log("PATH: " + thePath);
+    String quizDir = thePath + "/quizes";
+    HttpServlet myManager = (HttpServlet)config.getServletContext().getAttribute("manager");
+      myManager.getInitParameter("quizdir");
+    %>
+  </head>
+  <body>
+    <h1>Welcome to the Quiz Place</h1>
+  </body>
 </html>
